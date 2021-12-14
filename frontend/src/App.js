951ba1579245df211df1abda5f7a2b85f5f942f4
abhinavsiwal <abhinavsiwal@gmail.com>
2021-12-14 13:12:22 +0530

    Frontend Completes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,46 @@
+import { useState, useEffect } from "react";
+import "./App.css";
 
-import './App.css';
+import { io } from "socket.io-client";
+import { nanoid } from "nanoid";
 
+const socket = io.connect("http://localhost:5000");
+const userName = nanoid(4);
 function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+
+  const sendChat=e=>{
+    e.preventDefault();
+    socket.emit("chat",{message,userName});
+    setMessage('');
+  }
+
+  useEffect(() => {
+    socket.on("chat",(payload)=>{
+      setChat([...chat,payload])
+    })
+  }, [chat])
+
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Chatty app</h1>
+        <h1>Chatty app</h1>
+        {chat.map((payload,index)=>{
+          return(
+            <p key={index}>{payload.message}:<span>id:{payload.userName}</span></p>
+          )
+        })}
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="send text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
       </header>
     </div>
   );
